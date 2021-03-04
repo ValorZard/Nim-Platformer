4e84a4b7e8e9da8394ee1d4f090c75864893b11c
Srayan Jana <srayan.jana@outlook.com>
2021-03-03 16:07:21 -0800

    stuff is completely broken

--- src/main.nim
+++ src/main.nim
@@ -1,58 +1,308 @@
 import nico
+import nico/vec
 import bumpy
-import strformat
-import fpn
+# import strformat
+import polymorph
 # moves when you do - theme
 # control two platforming characters at the same time, and get them into their respective holes
 # this shouldn't be messy at all :)
-genQM_N(initQ22_10, 10, fixedPoint32)
-
-# player states
-type States {.pure.} = enum # can only use what i explicitly typed out
-  Moving, Ground, Turnaround, Jump, Air, AirMoving
 
 # base types
 type
-  Vec2i = object 
-    x, y: int
-  GameObject = ref object of RootObj
-    position: Vec2i
-  RectObj = ref object of GameObject
-    width, height: int
-  RectHitbox = ref object of RootObj
-    transform : Rect
-    isSolid : bool
-  Player = ref object of RectObj
-    state : States
-    facingRight : bool
-    hitbox: RectHitbox
-    velocity: Vec2i
-    # horizontal physics
-    groundAccel: int
-    maxGroundSpeed: int
-    # vertical physics
-    airAccel: int
-    maxAirSpeed: int
-    jumpForce: int
-    airJumpsLeft: int
-    maxAirJumps: int
-    terminalVelocity: int
-    gravity: int
-    # friction
-    staticFriction: int # should be strong to stop quickly
-    kineticFriction: int # should be weak to facilitate movement
-    drag: int # air friction
+  States {.pure.} = enum # can only use what i explicitly typed out
+    Moving, Ground, Turnaround, Jump, Air, AirMoving
+### ECS REWRITE
 
-  PlayerInput = ref object of RootObj
-    pressedLeft, pressedRight, jumpPressed : bool
+registerComponents(defaultComponentOptions):
+  type
+    # Tag
+    Player = object
+    # States
+    State = object 
+      currentState : States
+    # Components
+    Position = object
+      position : Vec2i
+    Rectangle = object
+      width, height: int
+    Hitbox = object
+      transform : Rect
+      isSolid : bool
+    FacingRight = distinct bool
+    Velocity = object
+      value : Vec2i
+    Acceleration = object 
+      # horizontal physics
+      groundAccel : int
+      maxGroundSpeed : int
+      # vertical physics
+      airAccel : int
+      maxAirSpeed: int
+    Gravity = object 
+      terminalVelocity: int
+      gravity: int
+    Friction = object
+      staticFriction: int # should be strong to stop quickly
+      kineticFriction: int # should be weak to facilitate movement
+      drag: int # air friction
+    JumpComponent = object
+      # universal jump stuff
+      jumpForce : int
+      # air jumps
+      airJumpsLeft: int
+      maxAirJumps: int
+    PlayerInput = ref object of RootObj
+      pressedLeft, pressedRight, jumpPressed : bool
+
+
+# Create systems to act on the components
+
+# system definitions
+
+
+defineSystem("HitboxCollision", [Hitbox], defaultSystemOptions):
+  player: EntityRef
+
+
+# actual system implementations
+
+makeSystem("PlayerSetInput", [Player, PlayerInput]):
+  # `item` is generated from the types passed within `[]`
+  init:
+    sys.paused = true
+  all:
+    item.playerInput.pressedLeft = btn(pcLeft)
+    item.playerInput.pressedRight = btn(pcRight)
+    item.playerInput.jumpPressed = btnp(pcA)
+  finish:
+    sys.paused = true
+
+makeSystem("PlayerMoveX", [Player, State, Velocity, Acceleration, PlayerInput]):
+  init:
+    sys.paused = true
+  all:
+    let accel =
+      case item.state.currentState:
+      of States.Moving: item.acceleration.groundAccel
+      of States.AirMoving: item.acceleration.airAccel
+      else: 0
+  
+    let maxSpeed =
+      case item.state.currentState:
+      of States.Moving: item.acceleration.maxGroundSpeed
+      of States.AirMoving: item.acceleration.maxAirSpeed
+      else: 0
+  
+    if item.playerInput.pressedLeft:
+      item.velocity.value.x -= (if item.velocity.value.x - accel >= -maxSpeed: accel else: 0)
+    if item.playerInput.pressedRight:
+      item.velocity.value.x += (if item.velocity.value.x + accel <= maxSpeed: accel else: 0)
+  finish:
+    sys.paused = true
+makeSystem("PlayerFriction", [Player, State, Velocity, Friction]):
+  init:
+    sys.paused = true
+  all:
+    let friction = 
+      case item.state.currentState:
+      of States.Moving: item.friction.kineticFriction
+      of States.Ground, States.Turnaround: item.friction.staticFriction
+      of States.Air, States.AirMoving: item.friction.drag
+      else: 0
+    if item.velocity.value.x < 0:
+      if item.velocity.value.x + friction > 0:
+        item.velocity.value.x = 0
+      else:
+        item.velocity.value.x += friction
+    elif item.velocity.value.x > 0:
+      if item.velocity.value.x - friction < 0:
+        item.velocity.value.x = 0
+      else:
+        item.velocity.value.x -= friction
+  finish:
+    sys.paused = true
+makeSystem("PlayerJump", [Player, JumpComponent, Velocity]):
+  init:
+    sys.paused = true
+  all:
+    item.velocity.value.y = -item.jumpComponent.jumpForce
+  finish:
+    sys.paused = true
+
+makeSystem("PlayerGravity", [Player, Gravity, Velocity]):
+  init:
+    sys.paused = true
+  all:
+    item.velocity.value.y += (if item.velocity.value.y + item.gravity.gravity <= item.gravity.terminalVelocity: item.gravity.gravity else: 0)
+  finish:
+    sys.paused = true
+
+# Have to pass the options into the system.
+makeSystemOpts("HitboxCollision", [Hitbox], defaultSystemOptions):
+  init:
+    sys.paused = true
+  all:
+    var isColliding = false
+    if(item != player):
+      if(overlap(player.hitbox.transform, item.hitbox.transform)):
+        isColliding = true
+    
+    if isColliding:
+      player.velocity.value.y = 0 
+      player.state.currentState = States.Ground
+      player.jumpComponent.airJumpsleft = player.jumpComponent.maxAirJumps
+    else:
+      player.state.currentState = States.Air
+
+
+#[
+makeSystemOptFields("HitboxCollision", [Hitbox], defaultSystemOptions) do:
+  player: EntityRef
+do:
+  init:
+    sys.paused = true
+  all:
+    var isColliding = false
+    if(item != player):
+      if(overlap(player.hitbox.transform, item.hitbox.transform)):
+        isColliding = true
+    
+    if isColliding:
+      player.velocity.value.y = 0 
+      player.state.currentState = States.Ground
+      player.jumpComponent.airJumpsleft = player.jumpComponent.maxAirJumps
+    else:
+      player.state.currentState = States.Air
+]#
+
+
+makeSystem("PlayerCheckCollision", [Player, Velocity, JumpComponent, Hitbox]):
+  init: 
+    sys.paused = true
+  all:
+    hitboxCollision.player = item
+    doHitboxCollision()
+  finish:
+    sys.paused = true
+
+
+makeSystem("PlayerApplyVelocity", [Player, Position, Velocity]):
+  init:
+    sys.paused = true
+  all:
+    item.position.position.x += item.velocity.value.x
+    item.position.position.y += item.velocity.value.y
+  finish:
+    sys.paused = true
+
+makeSystem("PlayerUpdateHitbox", [Player, Position, Hitbox]):
+  init:
+    sys.paused = true
+  all:
+    item.hitbox.transform.x = float item.position.x
+    item.hitbox.transform.y = float item.position.y
+  finish:
+    sys.paused = true
+
+makeSystem("PlayerUpdate", [Player, PlayerInput, State, JumpComponent]):
+  init:
+    sys.paused = true
+  all:
+    doPlayerSetInput()
+
+    template changeToGroundJump =
+      if (item.playerInput.jumpPressed):
+        item.state.currentState = States.Jump
+
+    template checkForAirJump =
+      if (item.jumpComponent.airJumpsLeft > 0 and item.playerInput.jumpPressed):
+        item.jumpComponent.airJumpsLeft -= 1
+        item.state.currentState = States.Jump
+
+
+    case item.state.currentState:
+      of States.Ground:
+        #echo "Ground"
+        doPlayerFriction()
+
+        if (item.playerInput.pressedLeft or item.playerInput.pressedRight):
+          item.state.currentState = States.Moving
+      
+        doPlayerCheckCollision()
+        changeToGroundJump()
+      of States.Moving:
+        doPlayerMoveX()
+        doPlayerFriction(player)
+
+        if (item.playerInput.pressedLeft or item.playerInput.pressedRight):
+        # the follow is to facilitate non slippery turnarounds.
+          if(item.playerInput.pressedRight != item.facingRight):
+            item.state.currentState = States.Turnaround
+            item.facingRight = item.playerInput.pressedRight
+        else:
+          item.state.currentState = States.Ground
+
+        doPlayerCheckCollision()
+        changeToGroundJump()
+      of States.Turnaround:
+        doPlayerMoveX()
+        doPlayerFriction()
+
+        if (item.playerInput.pressedLeft or item.playerInput.pressedRight):
+          item.state.currentState = States.Moving
+        else:
+          item.state.currentState = States.Ground
+      
+        doPlayerCheckCollision()
+        changeToGroundJump()
+      of States.Jump:
+        doPlayerJump()
+        item.state.currentState = States.Air
+      of States.Air:
+        #echo "Air"
+        doPlayerFriction()
+        doPlayerGravity()
+
+        if (item.playerInput.pressedLeft or item.playerInput.pressedRight):
+          item.state.currentState = States.AirMoving
+      
+        doPlayerCheckCollision()
+        checkForAirJump()
+      of States.AirMoving:
+        doPlayerMoveX()
+        doPlayerFriction()
+        doPlayerGravity()
+
+        if not (item.playerInput.pressedLeft or item.playerInput.pressedRight):
+          item.state.currentState = States.Air
+      
+        doPlayerCheckCollision()
+        checkForAirJump()
+  
+    doPlayerApplyVelocity()
+    doPlayerUpdateHitbox()
+  finish:
+    sys.paused = true
+
+# Seal and generate ECS
+makeEcs()
+commitSystems("run")
+
+### END ECS REWRITE
 
 # global variables (used for now)
 
-var player : Player
-var playerInput : PlayerInput
+let 
+  player = newEntityWith(
+      Position(
+        position : Vec2i(
+          x : screenWidth div 2,
+          y : screenHeight div 2
+        ),
+      ),
+    )
 
-var randomBox : RectHitbox
-
+#[
 proc playerInit() =
   player = Player(
 
@@ -100,6 +350,7 @@ proc playerInit() =
   playerInput.pressedLeft = false
   playerInput.pressedRight = false
   playerInput.jumpPressed = false
+]#
 
 proc gameInit() =
   loadFont(0, "font.png")
@@ -119,156 +370,6 @@ proc gameInit() =
     ),
     isSolid : true,
   )
-
-proc playerSetInput(player : Player, playerInput: PlayerInput) =
-  playerInput.pressedLeft = btn(pcLeft)
-  playerInput.pressedRight = btn(pcRight)
-  playerInput.jumpPressed = btnp(pcA)
-
-
-proc playerMoveX(player : Player, playerInput: PlayerInput) =
-  let accel =
-    case player.state:
-    of States.Moving: player.groundAccel
-    of States.AirMoving: player.airAccel
-    else: 0
-  
-  let maxSpeed =
-    case player.state:
-    of States.Moving: player.maxGroundSpeed
-    of States.AirMoving: player.maxAirSpeed
-    else: 0
-  
-  if playerInput.pressedLeft:
-    player.velocity.x -= (if player.velocity.x - accel >= -maxSpeed: accel else: 0)
-  if playerInput.pressedRight:
-    player.velocity.x += (if player.velocity.x + accel <= maxSpeed: accel else: 0)
-
-
-proc playerFriction(player : Player) =
-  let friction = 
-    case player.state:
-    of States.Moving: player.kineticFriction
-    of States.Ground, States.Turnaround: player.staticFriction
-    of States.Air, States.AirMoving: player.drag
-    else: 0
-  if player.velocity.x < 0:
-    if player.velocity.x + friction > 0:
-      player.velocity.x = 0
-    else:
-      player.velocity.x += friction
-  elif player.velocity.x > 0:
-    if player.velocity.x - friction < 0:
-      player.velocity.x = 0
-    else:
-      player.velocity.x -= friction
-
-
-proc playerJump(player : Player) =
-  player.velocity.y = -player.jumpForce
-
-
-
-proc playerGravity(player : Player) =
-  player.velocity.y += (if player.velocity.y + player.gravity <= player.terminalVelocity: player.gravity else: 0)
-
-proc playerCheckCollision(player : Player, randomBox : RectHitbox) : bool =
-  if overlaps(player.hitbox.transform, randomBox.transform):
-    player.velocity.y = 0
-    return true
-  else:
-    return false
-
-proc playerApplyVelocity(player : Player, dt: float32) =
-  player.position.x += player.velocity.x
-  player.position.y += player.velocity.y
-
-proc playerUpdateHitbox(player: Player) =
-  player.hitbox.transform.x = float player.position.x
-  player.hitbox.transform.y = float player.position.y
-
-proc playerUpdate(player: Player, dt: float32) =
-  playerSetInput(player, playerInput)
-
-  template changeToGroundJump =
-    if (playerInput.jumpPressed):
-      player.state = States.Jump
-
-  template checkForAirJump =
-    if (player.airJumpsLeft > 0 and playerInput.jumpPressed):
-      player.airJumpsLeft -= 1
-      player.state = States.Jump
-
-  template checkForGround =
-    if playerCheckCollision(player, randomBox):
-      player.state = States.Ground
-      player.airJumpsLeft = player.maxAirJumps
-  
-  template checkForAir = 
-    if not playerCheckCollision(player, randomBox):
-      player.state = States.Air
-
-  case player.state:
-    of States.Ground:
-      #echo "Ground"
-      playerFriction(player)
-
-      if (playerInput.pressedLeft or playerInput.pressedRight):
-        player.state = States.Moving
-      
-      checkForAir()
-      changeToGroundJump()
-    of States.Moving:
-      playerMoveX(player, playerInput)
-      playerFriction(player)
-
-      if (playerInput.pressedLeft or playerInput.pressedRight):
-      # the follow is to facilitate non slippery turnarounds.
-        if(playerInput.pressedRight != player.facingRight):
-          player.state = States.Turnaround
-          player.facingRight = playerInput.pressedRight
-      else:
-        player.state = States.Ground
-
-      checkForAir()
-      changeToGroundJump()
-    of States.Turnaround:
-      playerMoveX(player, playerInput)
-      playerFriction(player)
-
-      if (playerInput.pressedLeft or playerInput.pressedRight):
-        player.state = States.Moving
-      else:
-        player.state = States.Ground
-      
-      checkForAir()
-      changeToGroundJump()
-    of States.Jump:
-      playerJump(player)
-      player.state = States.Air
-    of States.Air:
-      #echo "Air"
-      playerFriction(player)
-      playerGravity(player)
-
-      if (playerInput.pressedLeft or playerInput.pressedRight):
-        player.state = States.AirMoving
-      
-      checkForGround()
-      checkForAirJump()
-    of States.AirMoving:
-      playerMoveX(player, playerInput)
-      playerFriction(player)
-      playerGravity(player)
-
-      if not (playerInput.pressedLeft or playerInput.pressedRight):
-        player.state = States.Air
-      
-      checkForGround()
-      checkForAirJump()
-  
-  playerApplyVelocity(player, dt)
-  playerUpdateHitbox(player)
 
 proc gameUpdate(dt: float32) =
   let dt = 1/60 # fixed 6 frames a second
@@ -292,7 +393,7 @@ proc gameDraw() =
 
   # render text
   var debugStr : string = fmt"Pos: ({player.position.x}, {player.position.y})"
-  case player.state:
+  case item.state.currentState:
     of States.Ground: debugStr.add("State: Ground")
     of States.Moving: debugStr.add("State: Moving")
     of States.Turnaround: debugStr.add("State: Turnaround")
